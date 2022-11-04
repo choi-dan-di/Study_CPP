7fe8476019007807b0275e80e451dc8698b1965e
choi-dan-di <tanvely@kakao.com>
2022-11-05 02:35:15 +0900

    upload variable

---
+++ variable/bool_and_float.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+// 불리언(boolean) : 참/거짓을 나타내는 타입
+bool isHighLevel = true;
+bool isPlayer = true;
+bool isMale = false;
+
+// 실수 (부동소수점)
+float attackSpeed = -3.375f; // 4바이트
+double attackSpeed2 = 123.4123; // 8바이트
+
+int main()
+{
+    //  cout << isHighLevel << endl;
+    cout << isHighLevel << endl;
+}
