604f0276da07aa62a349d6190fc6a139943eeb39
choi-dan-di <tanvely@kakao.com>
2022-11-16 20:32:31 +0900

    upload call-stack, function-final

---
+++ function/call-stack/call-stack/call-stack.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+
+// 호출 스택
+
+// 함수 선언
+// 매개 변수 이름은 생략 가능하지만 
+// 가능한 어떻게 사용되어지는지를 알게 하기위해 정해두는 편이 좋다.
+void Func1();
+void Func2(int a, int b);
+// void Func2(int, int);
+void Func3(float a);
+// void Func3(float);
+
+int main()
+{
+    cout << "main" << endl;
+    Func1();
+}
+
+void Func1()
+{
+    cout << "Func1" << endl;
+
+    // push 2
+    // push 1
+    // call Func2
+    Func2(1, 2);
+
+    Func3(10.0f);
+}
+
+/*
+void Func2(int a, int b)
+{
+    cout << "Func2" << endl;
+
+    Func3(10);
+}
+*/
+
+void Func3(float a)
+{
+    cout << "Func3" << endl;
+}
---
+++ function/function-final/function-final/function-final.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+using namespace std;
+
+// 함수 마무리
+
+// 오버로딩 (중복 정의 : 함수 이름의 재사용)
+int Add(int a, int b)
+{
+    int result = a + b;
+    return result;
+}
+
+/*
+float AddFloat(float a, float b)
+{
+    float result = a + b;
+    return result;
+}
+*/
+// Add 함수 오버로딩
+float Add(float a, float b)
+{
+    float result = a + b;
+    return result;
+}
+
+int Add(int a)
+{
+    int result = a + 1;
+    return result;
+}
+
+// 기본 인자값
+void SetPlayerInfo(int hp, int mp, int attack, int guildId = 0, int castleId = 2)
+{
+    cout << "hp : " << hp << endl;
+    cout << "mp : " << mp << endl;
+    cout << "attack : " << attack << endl;
+    cout << "guildId : " << guildId << endl;
+    cout << "castleId : " << castleId << endl;
+}
+
+// 스택 오버플로우
+int Factorial(int n)
+{
+    if (n <= 1)
+        return 1;
+
+    return n * Factorial(n - 1);
+}
+
+int main()
+{
+    float result = Add(1.5f, 2.1f);
+    //float resuldt = AddFloat(1.5f, 2.1f);
+    cout << result << endl;
+
+    cout << endl;
+
+    SetPlayerInfo(100, 40, 10, 1);
+
+    cout << endl;
+
+    int result2 = Factorial(5);
+    //int result2 = Factorial(1000000); // 스택 오버플로우
+    cout << result2 << endl;
+}
