8ca3bd00d4f78c14326bd63aa1af6be9e867e11a
choi-dan-di <tanvely@kakao.com>
2022-11-24 13:55:10 +0900

    upload cpp/multiple-pointer

---
+++ pointer/multiple-pointer/multiple-pointer/multiple-pointer.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+// 다중 포인터
+
+void SetMessage(const char* a) {
+	a = "Bye";
+}
+
+void SetMessage(const char** a) {
+	*a = "Bye";
+}
+
+void SetMessage2(const char*& a) {
+	a = "Ref Bye";
+}
+
+int main()
+{
+	const char* msg = "Hello";
+	SetMessage(msg);
+	cout << msg << endl;
+
+	// const char* 타입의 변수를 넘겨줘야 한다.
+	const char** pp = &msg;
+	//*pp = "Bye";
+	SetMessage(&msg);
+	cout << msg << endl;
+
+	SetMessage2(msg);
+	cout << msg << endl;
+
+	return 0;
+}
