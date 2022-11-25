87cf43369d6150ce1ed1957d0c8c54ad15685cba
choi-dan-di <tanvely@kakao.com>
2022-11-25 21:58:52 +0900

    pointer/practice-string, snail

---
+++ pointer/practice/snail/snail/snail.cpp
@@ -0,0 +1,106 @@
+﻿#include <iostream>
+#include <iomanip>  // 숫자 두 자리로 포맷 해주기 위해
+using namespace std;
+
+// 연습 문제 : 달팽이
+// 달팽이처럼 소용돌이치는 듯이 정렬된 숫자 배열 출력해보기
+
+const int MAX = 100;
+int board[MAX][MAX] = {};
+int N;
+
+enum DIR {
+    RIGHT = 0,
+    DOWN = 1,
+    LEFT = 2,
+    UP = 3
+};
+
+void PrintBoard() {
+    for (int y = 0; y < N; y++) {
+        for (int x = 0; x < N; x++) {
+            cout << setfill('0') << setw(2) << board[y][x] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void SetBoard() {
+    int dir = RIGHT;
+    int num = 1;
+    int y = 0;
+    int x = 0;
+
+    int dy[4] = { 0, 1, 0, -1 };
+    int dx[4] = { 1, 0, -1, 0 };
+
+    while (true) {
+        board[y][x] = num;
+
+        if (num == N * N) break;
+
+        int ny = y + dy[dir];
+        int nx = x + dx[dir];
+        /*
+        switch (dir)
+        {
+        case RIGHT:
+            ny = y;
+            nx = x + 1;
+            break;
+        case DOWN:
+            ny = y + 1;
+            nx = x;
+            break;
+        case LEFT:
+            ny = y;
+            nx = x - 1;
+            break;
+        case UP:
+            ny = y - 1;
+            nx = x;
+            break;
+        }
+        */
+
+        // 이동할 수 있는지 체크
+        // 이동하지 못하면 방향 전환
+        if (ny < 0 || ny >= N || nx < 0 || nx >= N || board[ny][nx] != 0) {
+            dir = (dir + 1) % 4;
+            /*
+            switch (dir) 
+            {
+            case RIGHT:
+                dir = DOWN;
+                break;
+            case DOWN:
+                dir = LEFT;
+                break;
+            case LEFT:
+                dir = UP;
+                break;
+            case UP:
+                dir = RIGHT;
+                break;
+            }
+            */
+            continue;
+        }
+
+        y = ny;
+        x = nx;
+        num++;
+    }
+ 
+}
+
+int main()
+{
+    cin >> N;
+
+    SetBoard();
+
+    PrintBoard();
+
+    return 0;
+}
---
+++ pointer/practice/string/string/string.cpp
@@ -0,0 +1,160 @@
+﻿#include <iostream>
+using namespace std;
+
+// 문자열 관련 함수 만들어보기
+
+// 1. 문자열의 길이를 반환해주는 함수
+int StrLen(const char* str) {
+    int ret = 0;
+
+    while (str[ret]) {
+        ret++;
+    }
+
+    return ret;
+}
+
+// 2. 문자열 복사 함수
+void StrCpy(char* dest, char* src) {
+    // src -> dest
+    int i = 0;
+    while (src[i]) {
+        dest[i] = src[i];
+        i++;
+    }
+
+    dest[i] = '\0';
+}
+
+// 포인터 사용 버전
+void StrCpyByPtr(char* dest, char* src) {
+    // src -> dest
+    char* ret = dest;
+
+    while (*src) {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+
+    *dest = '\0';
+
+    // 반환 타입이 있을 경우
+    //return ret;
+}
+
+// 3. 문자열 덧붙이는 함수
+void StrCat(char* dest, char* src) {
+    // dest 뒤에 src 붙이기
+    int i = 0;
+    int len = StrLen(dest);
+    while (src[i]) {
+        dest[len + i] = src[i];
+        i++;
+    }
+
+    dest[len + i] = '\0';
+}
+
+// 포인터 사용 버전
+void StrCatByPtr(char* dest, char* src) {
+    // dest 뒤에 src 붙이기
+    while (*dest) dest++;
+    while (*src) *dest++ = *src++;
+    *dest = '\0';
+}
+
+// 4. 두 개의 문자열 비교
+int StrCmp(char* a, char* b) {
+    int aLen = StrLen(a);
+    int bLen = StrLen(b);
+
+    if (aLen > bLen) return 1;
+    else if (aLen < bLen) return -1;
+
+    // 길이 같으면
+    for (int i = 0; i < aLen; i++) {
+        if (a[i] > b[i]) return 1;
+        else if (a[i] < b[i]) return -1;
+    }
+
+    return 0;
+}
+
+// 포인터 사용 버전
+int StrCmpByPtr(char* a, char* b) {
+    int aLen = StrLen(a);
+    int bLen = StrLen(b);
+
+    if (aLen > bLen) return 1;
+    else if (aLen < bLen) return -1;
+
+    // 길이 같으면
+    while (*a) {
+        if (*a > *b) return 1;
+        else if (*a < *b) return -1;
+
+        a++;
+        b++;
+    }
+
+    return 0;
+}
+
+// 5. 문자열을 뒤집는 함수
+void ReverseStr(char* str) {
+    // Hello World -> dlroW olleH
+    int len = StrLen(str);
+
+    for (int i = 0; i < len / 2; i++) {
+        int temp = str[i];
+        str[i] = str[len - 1 - i];
+        str[len - 1 - i] = temp;
+    }
+}
+
+// 포인터 사용 버전
+void ReverseStrByPtr(char* str) {
+    // Hello World -> dlroW olleH
+    int len = StrLen(str);
+
+    for (int i = 0; i < len / 2; i++) {
+        char temp = *(str + i);
+        *(str + i) = *(str + len - 1 - i);
+        *(str + len - 1 - i) = temp;
+    }
+}
+
+#pragma warning(disable: 4996)
+int main()
+{
+    const int BUF_SIZE = 100;
+
+    char a[BUF_SIZE] = "Hello World";
+    char b[BUF_SIZE] = "Hello";
+
+    //int len = strlen(a);
+    //int len2 = sizeof(a);
+    int len = StrLen(a);
+    cout << len << endl;
+    //cout << a;
+
+    // strcpy(b, a);
+    // StrCpy(b, a);
+    // StrCpyByPtr(b, a);
+    // for (char it : b) cout << it << " ";
+
+    // strcat(a, b);
+    // StrCat(a, b);
+    // for (char it : a) cout << it << " ";
+
+    // strcmp(a, b);
+    // int ret = StrCmp(a, b);
+    // int ret = StrCmpByPtr(a, b);
+    // cout << ret << endl;
+
+    ReverseStr(a);
+    cout << a << endl;
+
+    return 0;
+}
